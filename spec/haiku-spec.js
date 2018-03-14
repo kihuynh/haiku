82668171ab546e261fa8ea9f3789f614c72d220b
kimberly huynh and michael albers <michael@>
2018-03-14 11:47:03 -0700

    change countVowels to skip double vowels

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -3,7 +3,7 @@ import { Haiku } from './../src/haiku.js';
 
 describe('countVowels', function() {
   let newHaiku = new Haiku("haiku");
-  let vowelCount = 3;
+  let vowelCount = 2;
   let haikuLine = "haiku"
   it('takes in haiku line and returns the number of vowels', function(){
     expect(newHaiku.countVowels()).toEqual(vowelCount);
--- src/haiku.js
+++ src/haiku.js
@@ -11,10 +11,16 @@ export class Haiku {
       let vowelCount = 0;
       let haikuChar = this.lineOne.split('');
       console.log(haikuChar);
-      for (let i=0; i < haikuChar.length; i++){
-        console.log(i);
+      for (let i = 0; i < haikuChar.length; i++){
+        let doubleVowel = (i - 1);
         if (haikuChar[i].match(/[aeiou]/gi)){
-          vowelCount++;
+          console.log(haikuChar[i]);
+          if (haikuChar[i].match(/[aeiou]/gi) && haikuChar[doubleVowel].match(/[aeiou]/gi)){
+            i++;
+          }
+          else{
+            vowelCount++;
+          }
         };
       };
       console.log(vowelCount);
