5337b647f80505d55277b4f0173039e55f2bbff6
Kimberly Huynh and Michael Albers <michael@>
2018-03-15 15:24:03 -0700

    add some front end

--- src/main.js
+++ src/main.js
@@ -4,5 +4,15 @@ import 'bootstrap';
 import './styles.css';
 
 $(document).ready(function() {
-
+  $('#haiku-form').submit(function(event){
+    event.preventDefault();
+    let haiku = new Haiku($('#text-form').val())
+    haiku.wordSplitter();
+    haiku.countSyllableTotal();
+    if (haiku.lineSyllables === 17) {
+      $('.results').text("It's a haiku" + " " + haiku.lineSyllables);
+    } else {
+    $('.results').text("not a haiku" + " " + haiku.lineSyllables);
+    }
+  });
 });
